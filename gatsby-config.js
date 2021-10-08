6fc9bdd5b1d9682f3a95cc12e6de6735359146e6
Rajib Mondal <mondal021002rajib@gmail.com>
2021-10-08 13:26:38 +0530

    webapp completed

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     options: {
       fonts: [
         `Roboto`,
-        `Gemunu Libre`
+        `Gemunu Libre`,
         `source sans \pro:300,400,400i,700` // you can also specify font weights and styles
       ],
       display: 'swap'
